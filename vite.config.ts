a6a09bab29b030dc3c7be2e084b8caa941e11d0e
Anthony Portugal <anthonyportugalrobles@gmail.com>
2023-01-26 22:01:38 -0500

    🔧 add aliases in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,21 @@ export default defineConfig({
 	plugins: [vue()],
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			layouts: fileURLToPath(
+				new URL('./src/components/layouts', import.meta.url)
+			),
+			pages: fileURLToPath(new URL('./src/components/pages', import.meta.url)),
+			templates: fileURLToPath(
+				new URL('./src/components/templates', import.meta.url)
+			),
+			organisms: fileURLToPath(
+				new URL('./src/components/organisms', import.meta.url)
+			),
+			molecules: fileURLToPath(
+				new URL('./src/components/molecules', import.meta.url)
+			),
+			atoms: fileURLToPath(new URL('./src/components/atoms', import.meta.url))
 		}
 	}
 });
