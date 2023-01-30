143661827b577050fadc2355e07302790fc1232c
Anthony Portugal <anthonyportugalrobles@gmail.com>
2023-01-29 19:57:13 -0500

    💄 add auto catppuccin color scheme for main layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
+<template>
+	<MainLayout>
+		<HomeIndex />
+	</MainLayout>
+</template>
+
 <script setup lang="ts">
+	import MainLayout from 'layouts/MainLayout';
 	import HomeIndex from 'pages/HomeIndex';
 </script>
-
-<template>
-	<main>
-		<HomeIndex />
-	</main>
-</template>
--- src/components/layouts/MainLayout/MainLayout.vue
+++ src/components/layouts/MainLayout/MainLayout.vue
@@ -1,4 +1,32 @@
 <template>
-	<main class="l-mainLayout"></main>
+	<main
+		:class="[
+			'l-mainLayout flex h-screen w-screen items-center justify-center transition-colors',
+			catppuccinColorScheme
+		]">
+		<slot />
+	</main>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { computed, ref } from 'vue';
+
+	const darkSchemeMatchMedia = window.matchMedia(
+		'(prefers-color-scheme: dark)'
+	);
+
+	const hasDarkPreference = ref<boolean>(darkSchemeMatchMedia.matches);
+	const catppuccinColorScheme = computed<string>(() => {
+		return hasDarkPreference.value ? 'mocha' : 'latte';
+	});
+
+	darkSchemeMatchMedia.addEventListener('change', () => {
+		hasDarkPreference.value = darkSchemeMatchMedia.matches;
+	});
+</script>
+
+<style scoped>
+	.l-mainLayout {
+		color: theme('colors.text');
+		background: theme('colors.base');
+	}
+</style>
